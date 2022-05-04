676e8f4cfde6c6c41caeed769397ea9acfe3a90c
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-05-04 18:42:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import react from "react";
 
 import ReactDOM from "react-dom";
 
-const name = "Sanidhya";
-ReactDOM.render(<h1>Hello {name}</h1>, document.getElementById("root"));
+const fname = "Sanidhya";
+const lname = "Varshney";
+const myluckynumber = 3;
+ReactDOM.render(
+  <div>
+    <h1>Hello {`${fname} ${lname}`} !</h1>
+    <p> Your Lucky Number is {Math.floor(Math.random() * 10)}</p>
+  </div>,
+  document.getElementById("root")
+);
